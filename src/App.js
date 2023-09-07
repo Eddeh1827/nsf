29c588e699226ed8f51a2efe35f460741bfef0bc
Eddie Bickham <bickhame@gmail.com>
2023-09-07 08:17:57 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+      {/*Sign-up form start*/}
+      <h1>Stay updated!</h1>
+      <p>Join 60,000+ product managers receiving monthly updates on:</p>
+      <p>
+        Product discovery and building what matters Measuring to ensure updates
+        are a success And much more!
+      </p>
+      <p>Email address</p>
+      <p>email@company.com</p>
+      <p>Subscribe to monthly newsletter</p>
+      {/*Sign-up form end*/}
+      {/*Success message start*/}
+      <p>Thanks for subscribing!</p>
+      <p>A confirmation email has been sent to ash@loremcompany.com.</p>
+      <p>
+        Please open it and click the button inside to confirm your subscription.
+      </p>
+      <p>Dismiss message</p>
+      {/*Success message end */}
+      <div class="attribution">
+        Challenge by{" "}
+        <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+          Frontend Mentor
         </a>
-      </header>
+        . Coded by <a href="#">Your Name Here</a>.
+      </div>
     </div>
   );
 }
